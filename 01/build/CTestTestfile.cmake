dc447adf286f3cb8b9a4fbadd43cfed3ec9a33d9
gCTRL26 <strela48loko@gmail.com>
2025-09-18 21:12:01 +0300

    Remove build

--- 01/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/MAI_OOP_Labs/01
-# Build directory: /workspaces/MAI_OOP_Labs/01/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(start_tests "/workspaces/MAI_OOP_Labs/01/build/start_tests")
-set_tests_properties(start_tests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/MAI_OOP_Labs/01/CMakeLists.txt;32;add_test;/workspaces/MAI_OOP_Labs/01/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
